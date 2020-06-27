6d656c388f1bdc792564e9cc71669f429ca7efbc
pvpender <59583646+pvpender@users.noreply.github.com>
2020-06-27 12:30:23 +0300

    Add files via upload

---
+++ Men_sc.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.ComponentModel;
+
+public class Men_sc : MonoBehaviour
+{
+    private int scen, menu;//будешь обращаться к ним из другого скрипта - поменяй private на public
+    public Button st, men, ext;
+    public GameObject ob,me;//либо грузишь сцену с меню, либо объеденяешь объекты связанный с меню под один gameobject (смотри как сделано меню) и задаешь им состояние setactive(false);
+    // Start is called before the first frame update
+    void Start()
+    {
+        //me.SetActive(false);
+        scen = 1;//Номер сцены с выбором персонажа
+        menu = 2;//Номер сцены с меню 
+    }
+    
+    public void start()
+    {
+        SceneManager.LoadScene(scen);
+    }
+    public void lo_men()
+    {
+       // SceneManager.LoadScene(menu);//либо грузишь сцену, либо смотри вариант 2
+         ob.SetActive(false); //одно из двух
+         me.SetActive(true);
+          
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ звук.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class звук : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider sl;
+    void Start()
+    {
+        sl.value = 0.5f; 
+    }
+    public GameObject me, st_me,tex;
+    public Text zn;
+
+    // Update is called once per frame
+  public void zvuk(float val)
+    {
+        float stval = AudioListener.volume;
+        int a;
+        string b;
+        stval = val;
+        AudioListener.volume = stval;
+        a=System.Convert.ToInt32(val*100);
+        //  zn = System.Convert.ToString(val) ;
+        b = System.Convert.ToString(a);
+        tex.GetComponent<Text>().text = b;
+        Debug.Log(a);
+    }
+    public void za()
+    {
+        me.SetActive(false);
+        st_me.SetActive(true);
+    }
+    
+}
